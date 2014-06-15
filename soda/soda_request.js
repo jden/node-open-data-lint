6ca46df4a608ccc38e0fa7fc0fb06158e0fe0623
Sean Brewer <seabre986@gmail.com>
2014-06-15 01:29:33 -0400

    Begin implementation of needed SODA api.

---
+++ soda/soda_request.js
@@ -0,0 +1,38 @@
+var request = require('request')
+, Q = require('q');
+
+function SodaRequest(url, username, password, token) {
+  this.url = url;
+  this.username = username; 
+  this.password = password;
+  this.token = token;
+}
+
+SodaRequest.prototype.get = function(resource, params) {
+  var deferred = Q.defer()
+  , options = {
+    url: this.url + resource,
+    qs: params,
+    auth: {
+     user: this.username,
+     pass: this.password
+    },
+    headers: {
+      'X-App-Token': this.token,
+    },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    if (error) {
+      deferred.reject(new Error(error));
+    } else {
+      deferred.resolve(body);
+    }
+  });
+
+  return deferred.promise;
+
+}
+
+module.exports = SodaRequest
